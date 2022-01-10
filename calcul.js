b9667e21c5c364304850c87c34ad9ccce306d003
youngjeJO <yj01591@gmail.com>
2022-01-10 15:07:09 +0900

    eval함수 말고 함수 만들어서 구현

--- calcul.js
+++ calcul.js
@@ -2,14 +2,13 @@
 // 연산 클릭 시 casioHead에 출력
 // = 클릭 시 연산 결과값 casioHead와 casioInput에 출력
 
-console.log(document.querySelectorAll('.casio_num')[1].innerText);
 const btn_num = document.querySelectorAll('.casio_num');
 const numli = document.querySelector('#casioHead');
 const btn_oper = document.querySelectorAll('.casio_oper');
 const numResult = document.querySelector('#casioInput');
 
-let num1;
-let num2;
+let num1 = '';
+let num2 = '';
 let oper;
 for (let i = 0; i < btn_num.length; i++) {
   btn_num[i].onclick = function () {
@@ -29,30 +28,59 @@ for (let i = 0; i < btn_num.length; i++) {
         alert('숫자부터 입력하세요');
       }
     } else if (oper === '*' || oper === '/' || oper === '+' || oper === '-') {
-      num2 = btn_num[i].innerText;
-      numli.innerText += num2;
+      num2 = num2 + btn_num[i].innerText;
+      numli.innerText += btn_num[i].innerText;
       console.log(num2);
     } else {
       if (numResult.innerText) {
         numResult.innerText = '';
         numli.innerText = '';
-        num1 = btn_num[i].innerText;
-        numli.innerText += num1;
+        num1 = num1 + btn_num[i].innerText;
+        numli.innerText = num1;
       } else {
-        num1 = btn_num[i].innerText;
-        numli.innerText += num1;
+        num1 = num1 + btn_num[i].innerText;
+        numli.innerText = num1;
         console.log(num1);
       }
     }
   };
 }
+//계산 함수
+function sum(num1, num2) {
+  return Number(num1) + Number(num2);
+}
+function minus(num1, num2) {
+  return Number(num1) - Number(num2);
+}
+function division(num1, num2) {
+  return Number(num1) / Number(num2);
+}
+function multi(num1, num2) {
+  return Number(num1) * Number(num2);
+}
 
+//계산 버튼
 document.querySelector('#casioResult').onclick = function () {
-  numResult.innerText = eval(numli.innerText);
-  numli.innerText = eval(numli.innerText);
-  oper = '';
+  if (oper === '+') {
+    numResult.innerText = sum(num1, num2);
+    numli.innerText = sum(num1, num2);
+    oper = '';
+  } else if (oper === '-') {
+    numResult.innerText = minus(num1, num2);
+    numli.innerText = minus(num1, num2);
+    oper = '';
+  } else if (oper === '*') {
+    numResult.innerText = multi(num1, num2);
+    numli.innerText = multi(num1, num2);
+    oper = '';
+  } else if (oper === '/') {
+    numResult.innerText = division(num1, num2);
+    numli.innerText = division(num1, num2);
+    oper = '';
+  }
 };
 
+//reset 버튼
 document.querySelector('#casioReset').onclick = function () {
   numResult.innerText = '';
   numli.innerText = '';
@@ -60,39 +88,3 @@ document.querySelector('#casioReset').onclick = function () {
   num2 = '';
   oper = '';
 };
-
-// let num1="";
-// let num2="";
-// let oper="";
-// btn_num[i].onclick=function(){
-//   if(oper===("*" || "/" || "+" || "-")){
-//     oper=btn_num[i].innerText;
-//     numli.innerText+=oper;
-//     console.log(oper);
-//   }else if(oper!=("*" || "/" || "+" || "-")) {
-//     num1=btn_num[i].innerText;
-//     numli.innerText+=num1;
-//     console.log(num1);
-//   }else {
-//     num2=btn_num[i].innerText;
-//     numli.innerText+=num2;
-//     console.log(num2);
-//   }
-
-//   }
-// }
-
-// eval........
-
-// typeof(Number(btn_num[i].innerText))=="number"
-
-// for (let i = 0; i < btn_oper.length; i++) {
-//   let oper="";
-// btn_oper[i].onclick=function(){
-//   console.log(btn_oper[i].innerText);
-//   oper+=btn_oper[i].innerText;
-//   numli.innerText+=oper;
-//   num=0;
-
-// }
-// }
